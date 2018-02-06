2000449d0faff7bc2cdcad393f01e358fcc0048e
Shin Alexandre Koseki <sjinko@users.noreply.github.com>
2018-02-06 15:15:22 +0100

    Update setup.py
    Adding arguments “description”, “url”, "author”, "author_email”.
    Edit argument “license” from “MIT License” to “MIT”.
    Edit argument “packages” to “find_packages()” which automatically lists the packages needed to run the program.

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from distutils.core import setup
 setup(
     name='Demopoll',
     version='0.1dev',
-    packages=['databook',],
-    license='MIT License',
+    description='Scripts and material to analyze the effect of contextual characteristics on political agreement between local communities in Switzerland',
+    url='https://github.com/sjinko/demopoll',
+    author='Shin Alexandre Koseki',
+    author_email='kosekishin@gmail.com',
+    packages=find_packages(),
+    license='MIT',
     long_description=open('README.txt').read(),
 )
